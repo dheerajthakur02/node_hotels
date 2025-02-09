b70eb65b63d35122d0d145bca80ed0bd6b2546e0
Dheeraj Kumar Thakur <dk35118@gmail.com>
2025-02-09 15:15:41 +0530

    shifting mongodb to online server

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose");
 require("dotenv").config()
 //Define mongodb connection URL
 
-const mongoURL=process.env.MONGODB_URL_LOCAL
+const mongoURL=process.env.MONGODB_URL
 // const mongoURL=process.env.MONGODB_URL
 
 // depricted this thing
