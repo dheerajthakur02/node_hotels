cb4d40904583c74f01b1d91413807f4f0ad21b6e
Dheeraj Kumar Thakur <dk35118@gmail.com>
2025-02-12 08:23:36 +0530

    Authentication

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mongoose=require("mongoose");
 require("dotenv").config()
 
 //Define mongodb connection URL
-// const mongoURL=process.env.MONGODB_URL
-const mongoURL=process.env.MONGODB_URL_LOCAL
+const mongoURL=process.env.MONGODB_URL
+// const mongoURL=process.env.MONGODB_URL_LOCAL
 
 // depricted this thing
 // set up mongoDB connection
